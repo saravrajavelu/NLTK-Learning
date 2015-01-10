4e97eb72410e19eee2896609d13603995e2ff671
sarav <saravrajavelu@gmail.com>
2015-01-10 23:39:50 +0530

    Updated Predictor

---
+++ gender_predictor.py
@@ -0,0 +1,46 @@
+from nltk.corpus import names
+import nltk
+import random
+
+labeled_names = ([(name, 'male') for name in names.words('male.txt')] + [(name, 'female') for name in names.words('female.txt')])
+random.shuffle(labeled_names)
+
+
+def gender_features2(name):
+    features = {}
+
+    '''
+    features["firstletterVowel"] = name[0].lower()
+    features["last_letter"] = name[-1].lower()
+    for letter in 'abcdefghijklmnopqrstuvwxyz':
+        features["count(%s)" % letter] = name.lower().count(letter)
+        features["has(%s)" % letter] = (letter in name.lower())
+    '''
+
+    if name[0].lower() in 'aeiou':
+        features["firstletterVowel"] = 1
+        features["firstletterConsonant"] = 0
+    else:
+        features["firstletterVowel"] = 0
+        features["firstletterConsonant"] = 1
+
+    if name[-1].lower() in 'aeiou':
+        features["lastletterVowel"] = 1
+        features["lastletterConsonant"] = 0
+    else:
+        features["lastletterVowel"] = 0
+        features["lastletterConsonant"] = 1 
+
+
+
+    features["countVowels"] = name.lower().count('a') + name.lower().count('e') + name.lower().count('i') + name.lower().count('o') + name.lower().count('u')
+    features["nameLength"] = len(name)
+    features["countConsonants"] = features["nameLength"] - features["countVowels"]
+    
+    return features
+
+featuresets = [(gender_features2(n), gender) for (n, gender) in labeled_names]
+train_set, test_set = featuresets[500:], featuresets[:500]
+classifier = nltk.NaiveBayesClassifier.train(train_set)
+print(nltk.classify.accuracy(classifier, test_set))
+print(classifier.show_most_informative_features(10))
